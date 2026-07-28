c0b24a1a30202eebb5eb2af3561611c856f3b98c
Pjosue <pjosuework@gmail.com>
2026-07-28 18:04:06 -0500

    se agrega clase biblioteca

---
+++ src/modelos/Biblioteca.java
@@ -0,0 +1,45 @@
+package modelos;
+
+public class Biblioteca {
+    private String nombre;
+    private String ubicacion;
+    private Material[] materiales;
+    private int capacidadMaxima;
+    private int cantidadMateriales;
+
+    public Biblioteca(String nombre,String ubicacion,int capacidadMaxima, int cantidadMateriales){
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+        this.materiales = new Material[capacidadMaxima];
+        this.cantidadMateriales = cantidadMateriales;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public int getCapacidadMaxima() {
+        return capacidadMaxima;
+    }
+
+    public int getCantidadMateriales() {
+        return cantidadMateriales;
+    }
+
+    public Material[] getMateriales() {
+        return materiales;
+    }
+
+@Override
+    public String toString(){
+        return"Nombre: " + this.nombre + "\n" +
+            "Ubicación: " + this.ubicacion + "\n" +
+            "Capacidad Máxima: " + this.capacidadMaxima + "\n" +
+            "Cantidad de Materiales: " + this.cantidadMateriales + "\n";
+}
+}
+
